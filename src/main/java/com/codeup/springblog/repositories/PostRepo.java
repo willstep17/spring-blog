91cdc94ba3befd8c3d22c389bafb5f22e514c693
William Stephens <william.a.stephens17@gmail.com>
2020-03-21 23:42:09 -0500

    ,

--- src/main/java/com/codeup/springblog/repositories/PostRepo.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepo.java
@@ -4,5 +4,4 @@ import com.codeup.springblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepo extends JpaRepository<Post, Long> {
-    Post findPostById(long id);
 }
