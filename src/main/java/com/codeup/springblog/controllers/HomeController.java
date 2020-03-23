df736871d5a88f2d20d328697e5ed1e05981a9ab
William Stephens <william.a.stephens17@gmail.com>
2020-03-22 21:10:56 -0500

    .

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -3,9 +3,11 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HomeController {
-
     @GetMapping("/home")
     public String welcome() {
         return "home";
